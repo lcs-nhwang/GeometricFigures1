81831ac223e1f5b18f124d34a6200bbb136b0728
lcs-nhwang <sjnhwang@lcs.on.ca>
2024-10-28 21:39:53 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Nicholas Hwang on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
